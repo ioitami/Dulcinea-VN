4df78ffb14eb2e5531364a8e1106610ad60eef95
ioitami <ioitami@protonmail.com>
2026-09-07 00:04:26 +0800

    save page ui edit

---
+++ Assets/Scripts/SaveSystem/SaveLoadGameButton.cs
@@ -0,0 +1,65 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine.UI;
+
+public class SaveLoadGameButton : MonoBehaviour
+{
+    public bool isSaveBtn;
+    public int saveSlotNumber;
+    public Image thumbnailImage;
+    public Sprite emptySaveSprite;
+    public TextMeshProUGUI saveID_Text;
+    public TextMeshProUGUI chapterName_Text;
+    public TextMeshProUGUI saveTimeStamp_Text;
+
+    private void OnEnable()
+    {
+        RefreshButton();
+    }
+
+    private void RefreshButton()
+    {
+        SaveData data = GameSingleton.instance.gameStateManager.LoadSaveID(saveSlotNumber);
+
+        if (data == null)
+        {
+            thumbnailImage.sprite = emptySaveSprite;
+            saveID_Text.text = "";
+            chapterName_Text.text = "";
+            saveTimeStamp_Text.text = "";
+            return;
+        }
+
+        Sprite screenshot = GameSingleton.instance.gameStateManager.GetSaveScreenshotSprite(saveSlotNumber);
+
+        if (screenshot != null)
+            thumbnailImage.sprite = screenshot;
+        else
+            thumbnailImage.sprite = emptySaveSprite;
+
+        saveID_Text.text = "Save " + data.saveID.ToString();
+        chapterName_Text.text = data.chapterName;
+        saveTimeStamp_Text.text = data.saveTimeStamp;
+    }
+
+    public void OnSaveLoadButtonClicked()
+    {
+        if (isSaveBtn == true)
+        {
+            GameSingleton.instance.gameStateManager.Save(saveSlotNumber, OnSaveComplete);
+        }
+        else
+        {
+            GameSingleton.instance.gameStateManager.LoadGame(saveSlotNumber);
+        }
+
+    }
+
+    private void OnSaveComplete(SaveData data)
+    {
+        RefreshButton();
+    }
+
+}

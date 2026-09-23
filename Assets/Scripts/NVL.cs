6877fb583e57057d1c0857c9080df679e27fd36a
ioitami <ioitami@protonmail.com>
2026-09-23 20:55:16 +0800

    added canvasgroup to avl/nvl

--- Assets/Scripts/NVL.cs
+++ Assets/Scripts/NVL.cs
@@ -3,5 +3,8 @@ using UnityEngine;
 
 public class NVL : MonoBehaviour
 {
+    public Canvas nvlCanvas;
+    public CanvasGroup nvlCanvasGroup;
+    public Transform nvlChoiceContainer;
     public TextMeshProUGUI nvlText;
 }
